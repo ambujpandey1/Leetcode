b3e7922eadeafd9a34372b59596362fe5868762b
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-07-20 20:01:02 +0530

    Time: 2 ms (32.11%), Space: 42.1 MB (85.37%) - LeetHub

---
+++ 3334-apple-redistribution-into-boxes/3334-apple-redistribution-into-boxes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        Arrays.sort(capacity);
+        int count=0;
+
+      int sumApple=0;
+      for(int a:apple){
+        sumApple+=a;
+      }
+
+      for(int i=capacity.length-1 ;i>=0 && sumApple > 0;i--){
+        int c=capacity[i];
+        // if(sumApple==0) break;
+        sumApple-=c;
+        count++;
+        
+      }
+     return count;
+    }
+}

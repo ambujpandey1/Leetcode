3d7661328e9ab69c2cbe26cf2ed1c63be7cb8fcd
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-07-22 19:25:47 +0530

    Time: 1 ms (62.46%), Space: 46.5 MB (49.41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,41 +1,44 @@
 class Solution {
     public int trap(int[] height) {
-    //    int left=0;
-    //    int right=height.length-1;
-    //    int leftMax=0;
-    //    int rightMax=0;
-    //    int waterCount=0;
+       int left=0;
+       int right=height.length-1;
+       int leftMax=0;
+       int rightMax=0;
+       int waterCount=0;
 
-    //    while(left<right){
-    //     int min=Math.min(height[left],height[right]);
-    //     if(min==height[left]){
-    //        leftMax=min; 
-    //     }else{
-    //       rightMax=min;  
-    //     }
-    //    }
+       while(left<right){
+        leftMax=Math.max(leftMax,height[left]);
+        rightMax=Math.max(rightMax,height[right]);
+        if(leftMax<rightMax){
+           waterCount+=leftMax-height[left++];
+        }else{
+          waterCount+=rightMax-height[right--] ;
+        }
+       }
+
+       return waterCount;
     
-    int n=height.length;
-     int []leftMax=new int[n];
-     int []rightMax=new int[n];
-     leftMax[0]=height[0];
-     rightMax[n-1]=height[n-1];
-    int count=0;
+//     int n=height.length;
+//      int []leftMax=new int[n];
+//      int []rightMax=new int[n];
+//      leftMax[0]=height[0];
+//      rightMax[n-1]=height[n-1];
+//     int count=0;
     
-    for(int i=1;i<height.length;i++){
-        leftMax[i]=Math.max(leftMax[i-1],height[i]);
+//     for(int i=1;i<height.length;i++){
+//         leftMax[i]=Math.max(leftMax[i-1],height[i]);
        
         
-    }
-     for(int i=n-2;i>=0;i--){
-        rightMax[i]=Math.max(rightMax[i+1],height[i]);   
+//     }
+//      for(int i=n-2;i>=0;i--){
+//         rightMax[i]=Math.max(rightMax[i+1],height[i]);   
         
-    }
-    for(int i=0;i<height.length;i++){
-        int c=Math.min(leftMax[i],rightMax[i]);
-        int ct=c-height[i];
-        count+=ct;
-    }
-return count;
+//     }
+//     for(int i=0;i<height.length;i++){
+//         int c=Math.min(leftMax[i],rightMax[i]);
+//         int ct=c-height[i];
+//         count+=ct;
+//     }
+// return count;
     }
 }

25e4986e9c95c2bbdfa3eb53f0c1cac33c19ef3e
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-07-24 00:11:03 +0530

    Time: 1 ms (99.96%), Space: 44.5 MB (57.78%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<9;i++){
            if(col!=i && board[row][i]==board[row][col]) return false;
            if(row!=i && board[i][col]==board[row][col]) return false;
-           if( col!=(3*(col/3)+i%3)&&row!=(3*(row/3)+i/3) &&board[(3*(row/3)+i/3)][(3*(col/3)+i%3)]==board[row][col]) return false;
+           if( row!=(3*(row/3)+i/3)&&col!=(3*(col/3)+i%3) &&board[(3*(row/3)+i/3)][(3*(col/3)+i%3)]==board[row][col]) return false;
         }
         return true;
     }

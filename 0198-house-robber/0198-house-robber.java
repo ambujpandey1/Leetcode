39d3c73d3d56c5a0ead0f44abc2df3637ae6bf0d
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-07-27 18:54:09 +0530

    Time: 0 ms (100%), Space: 40.9 MB (88.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -13,17 +13,29 @@ class Solution {
         int n=nums.length;
         if(n==0) return 0;
         if(n==1) return nums[0];
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
+        // int[] dp=new int[n];
+        // Arrays.fill(dp,-1);
     //    return houseRober(nums,dp,n-1); 
        
-    dp[0]=nums[0];
-    dp[1]=Math.max(nums[0],nums[1]);
+    // dp[0]=nums[0];
+    // dp[1]=Math.max(nums[0],nums[1]);
+    // for(int i=2;i<n;i++){
+    //     int pick=nums[i]+dp[i-2];
+    //     int notpick=dp[i-1];
+    //     dp[i]=Math.max(pick,notpick);
+    // }
+    // return dp[n-1];
+
+     int prev2=nums[0];
+    int prev=Math.max(nums[0],nums[1]);
     for(int i=2;i<n;i++){
-        int pick=nums[i]+dp[i-2];
-        int notpick=dp[i-1];
-        dp[i]=Math.max(pick,notpick);
+        int pick=nums[i]+prev2;
+        int notpick=prev;
+        int curr=Math.max(pick,notpick);
+        prev2=prev;
+        prev=curr;
+
     }
-    return dp[n-1];
+    return  prev;
     }
 }

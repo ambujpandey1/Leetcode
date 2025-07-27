6cd3872c8ca2f297b7f1e188193059018c447680
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-07-27 20:11:37 +0530

    Time: 7 ms (11.4%), Space: 48.2 MB (8.15%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -16,7 +16,24 @@ class Solution {
          if(m==0 && n==0) return grid[0][0];
         
         int dp[][]=new int[m][n];
-        for(int []row:dp) Arrays.fill(row,-1);
-        return rec(grid,m-1,n-1,dp);
+        // for(int []row:dp) Arrays.fill(row,-1);
+        // return rec(grid,m-1,n-1,dp);
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0) dp[i][j]=grid[0][0];
+            else{
+                int up=grid[i][j];
+                if(i>0) up+=dp[i-1][j];
+                else up+=1000000000;
+                int left=grid[i][j];
+                if(j>0) left+=dp[i][j-1];
+                else left+=1000000000;
+
+                dp[i][j]=Math.min(up,left);
+            }
+        }
+       }
+
+        return dp[m-1][n-1];
     }
 }

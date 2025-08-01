f449ea894764902476f43c87a437c46bbdbb60bd
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-08-01 22:51:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (65.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,12 +3,27 @@ class Solution {
       int r=matrix.length;
       int c=matrix[0].length;
 
-      for(int i=0;i<r;i++ ){
-        for(int j=0;j<c;j++){
-         if(matrix[i][j]==target) return true;
+     int s=0;
+     int e=r-1;
+     while(s<=e){
+        int mid=s+(e-s)/2;
+        if(matrix[mid][0] <=target && matrix[mid][c-1]>=target){
+            int se=0;
+            int ee=c-1;
+            while(se<=ee){
+                int m=se+(ee-se)/2;
+                if(matrix[mid][m]==target) return true;
+                else if(matrix[mid][m]<target) se=m+1;
+                else ee=m-1;
+            }
+            return false;
         }
-
-      }  
-      return false;
+        else if(target >matrix[mid][c-1]){
+            s=mid+1;
+        }else{
+            e=mid-1;
+        }
+     }
+     return false;
     }
 }

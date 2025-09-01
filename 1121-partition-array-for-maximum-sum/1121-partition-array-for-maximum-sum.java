057453b4c916823aac719289d0d977788d939539
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-09-01 19:28:08 +0530

    Time: 7 ms (54.24%), Space: 42.1 MB (79.18%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -15,9 +15,28 @@ class Solution {
         return dp[idx]=maxAns;
     }
     public int maxSumAfterPartitioning(int[] arr, int k) {
+        // int n=arr.length;
+        // int dp[]=new int[n];
+        // Arrays.fill(dp,-1);
+        // return f(0,arr,n,k,dp);
+
         int n=arr.length;
-        int dp[]=new int[n];
-        Arrays.fill(dp,-1);
-        return f(0,arr,n,k,dp);
+        int dp[]=new int[n+1];
+
+        for(int idx=n-1;idx>=0;idx--){
+            int len=0;
+        int maxAns=Integer.MIN_VALUE;
+        int max=Integer.MIN_VALUE;
+       
+        for(int j=idx;j<Math.min(n,idx+k);j++){
+            len++;
+            max=Math.max(max,arr[j]);
+            int sum=(len*max)+dp[j+1];
+            maxAns=Math.max(maxAns,sum);
+        }
+        dp[idx]=maxAns;
+        }
+
+        return dp[0];
     }
 }

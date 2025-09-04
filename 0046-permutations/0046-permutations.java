6b471fc063631c25318cbb1a5ad368e53a29af0d
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-09-04 21:41:48 +0530

    Time: 1 ms (92.67%), Space: 44.6 MB (64.25%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> permutaionRec(int[] nums,List<Integer> ds,List<List<Integer>>ans, boolean[] freq ){
+      if(ds.size()==nums.length){
+        ans.add(new ArrayList<>(ds));
+        return ans;
+      }  
+
+      for(int i=0;i<nums.length;i++){
+        if(!freq[i]){
+            freq[i]=true;
+            ds.add(nums[i]);
+            permutaionRec(nums,ds,ans,freq);
+            ds.remove(ds.size()-1);
+            freq[i]=false;
+        }
+      }
+      return ans;
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ds=new ArrayList<>();
+        boolean freq[]=new boolean[nums.length];
+        return permutaionRec(nums,ds,ans,freq);
+    }
+}

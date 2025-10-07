69b990342db4ead04598d9c1d4fcacc5a1a8d9b5
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-10-08 00:12:42 +0530

    Time: 31 ms (98%), Space: 43 MB (22.75%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -24,10 +24,29 @@ class Solution {
             arr[i + 1] = nums[i];
         }
         
-        int[][] dp=new int[arr.length+1][arr.length+1];
-        for(int r[]:dp){
-            Arrays.fill(r,-1);
+        // int[][] dp=new int[arr.length+1][arr.length+1];
+        // for(int r[]:dp){
+        //     Arrays.fill(r,-1);
+        // }
+        // return maxC(1, n, arr,dp);
+
+        // Tabulation
+      
+      int[][] dp=new int[arr.length+2][arr.length+2];
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j) continue;
+                 int ans = Integer.MIN_VALUE;
+                for (int idx = i; idx <= j; idx++) {
+                  int cost = arr[i - 1] * arr[idx] * arr[j + 1]
+                     + dp[i][ idx - 1]
+                     + dp[idx + 1][ j];
+            ans = Math.max(ans, cost);
+            
+            }
+            dp[i][j]=ans;
+            }
         }
-        return maxC(1, n, arr,dp);
+        return dp[1][n];
     }
 }

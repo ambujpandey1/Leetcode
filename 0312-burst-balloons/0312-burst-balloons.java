532032029acf7397f0f4376c1d6ca19899b4e590
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-10-08 00:03:58 +0530

    Time: 112 ms (29.64%), Space: 42.6 MB (81.45%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -0,0 +1,33 @@
+class Solution {
+    int maxC(int i, int j, int[] nums,int[][] dp) {
+        if (i > j) return 0;  // base case
+
+        int ans = Integer.MIN_VALUE;
+        if(dp[i][j]!=-1) return dp[i][j];
+        for (int idx = i; idx <= j; idx++) {
+            int cost = nums[i - 1] * nums[idx] * nums[j + 1]
+                     + maxC(i, idx - 1, nums,dp)
+                     + maxC(idx + 1, j, nums,dp);
+            ans = Math.max(ans, cost);
+        }
+        return dp[i][j]=ans;
+    }
+
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
+
+        // copy original nums into arr starting at index 1
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = nums[i];
+        }
+        
+        int[][] dp=new int[arr.length+1][arr.length+1];
+        for(int r[]:dp){
+            Arrays.fill(r,-1);
+        }
+        return maxC(1, n, arr,dp);
+    }
+}

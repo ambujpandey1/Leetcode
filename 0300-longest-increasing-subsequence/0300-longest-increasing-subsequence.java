291afab119d69350ae3061f25d1578090501d2f5
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-10-12 22:11:31 +0530

    Time: 432 ms (5.08%), Space: 77.4 MB (20.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,41 +1,22 @@
 class Solution {
-    int lcsLen(int idx, int previdx, int[] arr, int[][] dp, int n) {
-        if (idx == n)
-            return 0;
-        if (dp[idx][previdx + 1] != -1)
-            return dp[idx][previdx + 1];
-        int len = lcsLen(idx + 1, previdx, arr, dp, n);
-        if (previdx == -1 || arr[idx] > arr[previdx]) {
-            len = Math.max(len, (1 + lcsLen(idx + 1, idx, arr, dp, n)));
+    int lis(int idx,int pidx,int[] arr,int n,int[][] dp){
+        if(idx==n) return 0;
+        
+        if(dp[idx][pidx+1]!=-1) return dp[idx][pidx+1];
+        int notTake=0+lis(idx+1,pidx,arr,n,dp);
+        int take=0;
+        if(pidx==-1 || arr[idx]>arr[pidx]){
+            take=1+lis(idx+1,idx,arr,n,dp);
         }
-
-        return dp[idx][previdx + 1] = len;
+       int max=Math.max(take,notTake);
+       return dp[idx][pidx+1]= max;
     }
-
     public int lengthOfLIS(int[] nums) {
-        // Memoization
-        // int n=nums.length;
-        // int[][] dp=new int[n][n];
-        // for(int[] row:dp){
-        //     Arrays.fill(row,-1);
-        // }
-        // return lcsLen(0,-1,nums,dp,n);
-
-        // Tabulation
-        int n=nums.length;
-        int[][] dp=new int[n+1][n+1];
-
-        for(int idx=n-1;idx>=0;idx--){
-            for(int previdx=idx-1;previdx>=-1;previdx--){
-                int len=dp[idx+1][previdx+1];
-        if(previdx==-1 || nums[idx]>nums[previdx] ){
-            len=Math.max(len,(1+dp[idx+1][idx+1]));
-        }
-
-        dp[idx][previdx + 1] = len;
-            }
-        }
-        return dp[0][-1+1];
-
+      int n=nums.length;
+      int dp[][]=new int[n+1][n+1];
+      for(int r[]:dp){
+        Arrays.fill(r,-1);
+      }
+      return  lis(0,-1,nums,n,dp);
     }
 }

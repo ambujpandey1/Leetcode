dd7ffad01e58e066d2f2b665b9e5e95d14c09901
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-11-03 09:53:10 +0530

    Time: 3 ms (95.21%), Space: 47 MB (7.09%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,24 @@
+class Solution {
+    int f(int i,int j,String word1, String word2,int[][] dp){
+          if(i<0) return j+1;
+          if(j<0) return i+1;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(word1.charAt(i)==word2.charAt(j)){
+            return dp[i][j]= 0+f(i-1,j-1,word1,word2,dp);
+        }
+        else{
+            return dp[i][j]= 1+ Math.min(f(i-1,j-1,word1,word2,dp),Math.min(f(i-1,j,word1,word2,dp),f(i,j-1,word1,word2,dp)));
+        }
+    }
+    public int minDistance(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+         
+         int dp[][]= new int[n][m];
+         for(int r[]:dp){
+            Arrays.fill(r,-1);
+         }
+        return f(n-1,m-1,word1,word2,dp);
+    }
+}

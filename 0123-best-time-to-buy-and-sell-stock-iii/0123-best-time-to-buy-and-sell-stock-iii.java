8d29ec4255ec78a27bc29d1be02d4696d8958ef1
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-11-03 19:47:57 +0530

    Time: 144 ms (12.08%), Space: 136.1 MB (5.02%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    int f(int i,int buy, int cap, int[] prices,int[][][] dp){
+        if(i==prices.length || cap==0) return 0;
+
+        int profit=0;
+        if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
+        if(buy==1){
+            profit=Math.max(-prices[i]+f(i+1,0,cap,prices,dp),0+f(i+1,1,cap,prices,dp));
+        }
+        else // sell
+        {
+          profit=Math.max(prices[i]+f(i+1,1,cap-1,prices,dp),0+f(i+1,0,cap,prices,dp));
+        }
+        return dp[i][buy][cap]= profit;
+    }
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int[][][] dp= new int[n][2][3];
+        
+         // Initialize the dp array with -1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return f(0,1,2,prices,dp);
+    }
+}

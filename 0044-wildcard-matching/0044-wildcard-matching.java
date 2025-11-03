7162684c642544e73f08e84b43ec35b34d076e76
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-11-03 10:48:30 +0530

    Time: 20 ms (52.62%), Space: 63.3 MB (5.11%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,44 @@
+class Solution {
+    boolean isAllStars(String p, int j) {
+        for (int i = 0; i <= j; i++) {
+            if (p.charAt(i) != '*')
+                return false;
+        }
+        return true;
+    }
+
+    boolean f(int i, int j, String s, String p, int[][] dp) {
+        if (i < 0 && j < 0)
+            return true;
+        if (j < 0 && i >= 0)
+            return false;
+        if (i < 0 && j >= 0)
+            return isAllStars(p, j);
+
+        if (dp[i][j] != -1)
+            return dp[i][j] == 1;
+        boolean ans;
+        if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?') {
+            ans = f(i - 1, j - 1, s, p, dp);
+
+        } else if (p.charAt(j) == '*') {
+            ans = f(i - 1, j, s, p, dp) || f(i, j - 1, s, p, dp);
+        } else {
+            ans = false;
+        }
+
+        dp[i][j] = ans ? 1 : 0;
+        return ans;
+    }
+
+    public boolean isMatch(String s, String p) {
+
+        int n = s.length();
+        int m = p.length();
+        int dp[][] = new int[n][m];
+        for (int r[] : dp) {
+            Arrays.fill(r, -1);
+        }
+        return f(n - 1, m - 1, s, p, dp);
+    }
+}

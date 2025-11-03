16919c83b52db2682a8d8bbba6ca6a82a0734124
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-11-03 10:52:01 +0530

    Time: 22 ms (45.03%), Space: 62.5 MB (5.11%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,6 +1,6 @@
 class Solution {
     boolean isAllStars(String p, int j) {
-        for (int i = 0; i <= j; i++) {
+        for (int i = 0; i < j; i++) {
             if (p.charAt(i) != '*')
                 return false;
         }
@@ -8,20 +8,20 @@ class Solution {
     }
 
     boolean f(int i, int j, String s, String p, int[][] dp) {
-        if (i < 0 && j < 0)
+        if (i == 0 && j == 0)
             return true;
-        if (j < 0 && i >= 0)
+        if (j == 0 && i > 0)
             return false;
-        if (i < 0 && j >= 0)
+        if (i== 0 && j > 0)
             return isAllStars(p, j);
 
         if (dp[i][j] != -1)
             return dp[i][j] == 1;
         boolean ans;
-        if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?') {
+        if (p.charAt(j-1) == s.charAt(i-1) || p.charAt(j-1) == '?') {
             ans = f(i - 1, j - 1, s, p, dp);
 
-        } else if (p.charAt(j) == '*') {
+        } else if (p.charAt(j-1) == '*') {
             ans = f(i - 1, j, s, p, dp) || f(i, j - 1, s, p, dp);
         } else {
             ans = false;
@@ -35,10 +35,10 @@ class Solution {
 
         int n = s.length();
         int m = p.length();
-        int dp[][] = new int[n][m];
+        int dp[][] = new int[n+1][m+1];
         for (int r[] : dp) {
             Arrays.fill(r, -1);
         }
-        return f(n - 1, m - 1, s, p, dp);
+        return f(n , m , s, p, dp);
     }
 }

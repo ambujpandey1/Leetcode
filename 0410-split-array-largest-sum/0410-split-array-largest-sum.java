be0365148f22c1d9ae56c89fe223ff275b75862c
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-12-11 00:41:10 +0530

    Time: 1 ms (53.15%), Space: 42.9 MB (40.4%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+     boolean isPossible(int[] weights, int capacity, int days) {
+        int currWeight = 0;
+        int requiredDays = 1;
+
+        for (int w : weights) {
+            if (currWeight + w > capacity) {
+                requiredDays++;
+                currWeight = w;
+            } else {
+                currWeight += w;
+            }
+        }
+        return requiredDays <= days;
+    }
+
+    public int splitArray(int[] nums, int k) {
+       
+        int low = 0, high = 0;
+
+        for (int w : nums) {
+            low = Math.max(low, w); // max weight
+            high += w;              // total weight
+        }
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (isPossible(nums, mid, k)) {
+                high = mid - 1;   // try smaller capacity
+            } else {
+                low = mid + 1;    // increase capacity
+            }
+        }
+        return low; 
+    }
+}

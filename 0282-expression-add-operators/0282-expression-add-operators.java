cd5286a236fbda09c687f0a679f27e236e114253
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-12-14 19:27:06 +0530

    Time: 137 ms (11.75%), Space: 53.1 MB (13.94%) - LeetHub

---
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -0,0 +1,29 @@
+class Solution {
+    private void solve(int index,String num,int target, String curr,List<String> ans, long prev, long res){
+        if(index==num.length()){
+            if(res==target) ans.add(curr);
+            return;
+        }
+
+        String st="";
+        long currRes=0;
+        for(int i=index;i<num.length();i++){
+            if(i>index && num.charAt(index)=='0') break;
+            st+=num.charAt(i);
+            currRes=currRes*10+(num.charAt(i)-'0');
+            if(index==0){
+                solve(i+1,num,target,st,ans,currRes,currRes);
+            }
+            else{
+                solve(i+1,num,target,curr+"+"+st,ans,currRes,res+currRes);
+                solve(i+1,num,target,curr+"-"+st,ans,-currRes,res-currRes);
+                solve(i+1,num,target,curr+"*"+st,ans,prev*currRes,res-prev+(prev*currRes));
+            }
+        }
+    }
+    public List<String> addOperators(String num, int target) {
+        List<String> ans=new ArrayList<>();
+        solve(0,num,target,"",ans,0,0);
+        return ans;
+    }
+}
